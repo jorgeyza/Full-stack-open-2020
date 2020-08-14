d74e2cf15993d650406695f60d66825c831629c3
Jorge Eyzaguirre <jorgeyza@gmail.com>
2020-08-14 11:31:08 -0500

    Solved exercises 2.8, 2.9 and 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const handleAdd = (event) => {
     event.preventDefault();
@@ -11,31 +21,37 @@ const App = () => {
     if (personDuplicated) {
       return alert(`${newName} is already added to phonebook`);
     }
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
 
-  const handleInput = (event) => {
+  const handleName = (event) => {
     setNewName(event.target.value);
   };
 
-  const numbers = persons.map((person) => (
-    <li key={person.name}>{person.name}</li>
-  ));
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilter = (event) => {
+    setNewFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAdd}>
-        <div>
-          name: <input name="name" onChange={handleInput} value={newName} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>{numbers}</div>
+      <Filter newFilter={newFilter} handleFilter={handleFilter} />
+      <h3>add a new</h3>
+      <PersonForm
+        handleAdd={handleAdd}
+        handleName={handleName}
+        handleNumber={handleNumber}
+        newName={newName}
+        newNumber={newNumber}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
   );
 };
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Person from "./Person";
+
+const Persons = ({ persons, newFilter }) => {
+  return (
+    <ul>
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(newFilter.toLowerCase())
+        )
+        .map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
+    </ul>
+  );
+};
+
+export default Persons;
