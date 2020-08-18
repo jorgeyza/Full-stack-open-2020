51a2e450d641a415d142308280545ca3ff8998f6
Jorge Eyzaguirre <jorgeyza@gmail.com>
2020-08-18 13:42:11 -0500

    Solve exercises 3.1, 3.2, 3.3, 3.4, 3.5 and 3.6

---
+++ part3/index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 10000) + 1;
+  return id;
+};
+
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+app.get("/info", (req, res) => {
+  res.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>
+  `);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "missing name or number",
+    });
+  }
+  const duplicatePerson = persons.some((person) => person.name === body.name);
+  if (duplicatePerson) {
+    return res.status(409).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  return res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
