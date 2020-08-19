8bdef4af38cc4e454cd7f5e7e260a77f3c6e525c
Jorge Eyzaguirre <jorgeyza@gmail.com>
2020-08-19 14:58:07 -0500

    Solve exercises 3.15, 3.16, 3.17 and 3.18

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -12,29 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
@@ -45,20 +22,24 @@ app.use(
   )
 );
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (req, res) => {
-  res.send(`
-  <p>Phonebook has info for ${persons.length} people</p>
+  Person.estimatedDocumentCount().then((numPersons) =>
+    res.send(`
+  <p>Phonebook has info for ${numPersons} people</p>
   <p>${new Date()}</p>
-  `);
+  `)
+  );
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
@@ -67,10 +48,10 @@ app.get("/api/persons/:id", (req, res) => {
         res.status(404).end();
       }
     })
-    .catch((error) => console.error("Could not fetch person", error.message));
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
   let person;
 
@@ -88,29 +69,36 @@ app.post("/api/persons", (req, res) => {
   person
     .save()
     .then((savedPerson) => res.json(savedPerson))
-    .catch((error) => console.error("Could not save person to db"));
+    .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
   const body = req.body;
   const id = req.params.id;
   Person.findOne({ _id: id }).then((duplicatePerson) => {
     duplicatePerson.number = body.number;
-    console.log("duplicatePerson changed number", duplicatePerson);
     duplicatePerson
       .save()
       .then((savedPerson) => res.json(savedPerson))
-      .catch((error) => console.error("Could not update person number"));
+      .catch((error) => next(error));
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => {});
