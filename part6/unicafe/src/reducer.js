af0d3b7f865e17191da7fbbf2df01ea962cdc157
Jorge Eyzaguirre <jorgeyza@gmail.com>
2020-08-30 23:20:27 -0500

    Solve exercises 6.1 and 6.2

--- part6/unicafe/src/reducer.js
+++ part6/unicafe/src/reducer.js
@@ -1,23 +1,23 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'GOOD':
-      return state
+      return { ...state, good: state.good + action.data.value };
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + action.data.value };
     case 'BAD':
-      return state
+      return { ...state, bad: state.bad + action.data.value };
     case 'ZERO':
-      return state
-    default: return state
+      return { good: 0, ok: 0, bad: 0 };
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
--- part6/unicafe/src/reducer.test.js
+++ part6/unicafe/src/reducer.test.js
@@ -1,35 +1,98 @@
-import deepFreeze from 'deep-freeze'
-import counterReducer from './reducer'
+import deepFreeze from 'deep-freeze';
+import counterReducer from './reducer';
 
 describe('unicafe reducer', () => {
   const initialState = {
     good: 0,
     ok: 0,
-    bad: 0
-  }
+    bad: 0,
+  };
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
+    const state = {};
     const action = {
-      type: 'DO_NOTHING'
-    }
+      type: 'DO_NOTHING',
+    };
 
-    const newState = counterReducer(undefined, action)
-    expect(newState).toEqual(initialState)
-  })
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
 
   test('good is incremented', () => {
     const action = {
-      type: 'GOOD'
-    }
-    const state = initialState
+      type: 'GOOD',
+      data: {
+        value: 1,
+      },
+    };
+    const state = initialState;
 
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
     expect(newState).toEqual({
       good: 1,
       ok: 0,
-      bad: 0
-    })
-  })
-})
+      bad: 0,
+    });
+  });
+
+  test('neutral is incremented', () => {
+    const action = {
+      type: 'OK',
+      data: {
+        value: 1,
+      },
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    });
+  });
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD',
+      data: {
+        value: 1,
+      },
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    });
+  });
+
+  test('properties are set to 0', () => {
+    const action = {
+      type: 'ZERO',
+      data: {
+        good: 0,
+        ok: 0,
+        bad: 0,
+      },
+    };
+    const state = {
+      good: 3,
+      ok: 2,
+      bad: 5,
+    };
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0,
+    });
+  });
+});
